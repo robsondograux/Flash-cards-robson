a9cdee568060cf08ee4d0dfe9b7a72973be5d641
robsondograux <145396621+robsondograux@users.noreply.github.com>
2024-10-24 14:57:43 -0300

    24/10

---
+++ assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futsal',
+    'Qual a duração de uma partida de futsal?',
+    'A partida de futsal dura 40 minutos, divididos em dois tempos de 20 minutos cada.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o satelite natural da terra?',
+    'Lua'
+)
+
+criaCartao(
+    'Ciencias',
+    'Quantos ossos tem um corpo humano?',
+    'tem 206 ossos'
+)
+
+criaCartao(
+    '',
+    'Pergunta',
+    'resposta'
+)
