8f71db7ba96c77b18ffedfc657134b114928bdcc
robsondograux <145396621+robsondograux@users.noreply.github.com>
2024-10-31 14:50:12 -0300

    31/10

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-function  criaCartao(categori, pergunta, resposta){
+function  criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let Cartao = document.createElement('article');
+    Cartao.className = 'cartao';
 
-    
+    Cartao.innerHTML = `
+                <div class="cartao__conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>${pergunta}</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>${resposta}</p>
+                    </div>
+                </div>
+                 `
+    container.appendChild(Cartao);
+
 }
